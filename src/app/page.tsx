4d60875208cc5ed721e707d5f755c0c05c47fde8
okanacer332 <acer.okanumut@hotmail.com>
2025-12-04 19:39:27 +0000

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Header } from '@/components/Header';
+import { Hero } from '@/components/Hero';
+import { Services } from '@/components/Services';
+import { Blog } from '@/components/Blog';
+import { StickyContact } from '@/components/StickyContact';
+import { Footer } from '@/components/Footer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Hero />
+      <Services />
+      <Blog />
+      <StickyContact />
+      <Footer />
+    </div>
+  );
+}
